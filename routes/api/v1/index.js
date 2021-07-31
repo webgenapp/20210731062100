c731be746f0bb269fe09aa7dad9e6b6bf18b3a10
webgenapp <87999663+webgenapp@users.noreply.github.com>
2021-07-31 18:21:13 +0200

    Create app using webgen.dev

---
+++ routes/api/v1/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const coffeesRouter = require('./coffees')
+router.use('/coffees', coffeesRouter)
+
+const icecreamsRouter = require('./icecreams')
+router.use('/icecreams', icecreamsRouter)
+
+const usersRouter = require('./users')
+router.use('/users', usersRouter)
+
+module.exports = router
